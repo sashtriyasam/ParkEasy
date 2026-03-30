87d5e88b87f9db4349647534f75e9f86524d13e1
Your Name <you@example.com>
2026-03-30 21:09:11 +0530

    FIX: Missing react-native-reanimated/plugin in babel.config.js

--- ParkEasyMobile/babel.config.js
+++ ParkEasyMobile/babel.config.js
@@ -3,8 +3,7 @@ module.exports = function (api) {
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      // Only add plugins here if needed, for example:
-      // 'react-native-reanimated/plugin'
+      'react-native-reanimated/plugin',
     ],
   };
 };
